a08911b127c1695976e5d90b04c71df0b6456f93
Barbara Tess <barbaratessn@gmail.com>
2024-03-12 15:36:14 +0300

    Assignment complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
-// create your App component here
+import { useEffect, useState } from 'react'
+
+function App() {
+    const [imageLink, setImageLink] = useState('')
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(async () => {
+        const response = await fetch('https://dog.ceo/api/breeds/image/random')
+        const data = await response.json()
+        setImageLink(data.message)
+        setIsLoading(false)
+    }, [])
+
+    if (isLoading) {
+        return <p>Loading...</p>
+    }
+    return (
+        <div>
+            <img
+                src={imageLink}
+                alt='A Random Dog'
+            />
+        </div>
+    )
+}
+
+export default App
